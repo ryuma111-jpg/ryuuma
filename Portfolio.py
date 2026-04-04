6cea1b85661e3c5b33c3f63dfcadfaef29614bce
ryuma111-jpg <ryuma20051115@gmail.com>
2026-04-04 14:33:17 +0900

    Add files via upload

--- Portfolio.py
+++ Portfolio.py
@@ -1,24 +1,28 @@
 import streamlit as st
 import cv2
 import numpy as np
-import os
+from PIL import Image
 
-image_path = "image/peach.jpg"
+
 
 st.title("画像一致判定プログラム")
 
-if os.path.exists(image_path):
-    target_img = cv2.imread(image_path)
-    compare_img = cv2.imread(image_path)
-
-    if target_img is not None:
-        height, width = target_img.shape[:2]
-        compare_img = cv2.resize(compare_img, (width, height))
-        diff = cv2.absdiff(target_img, compare_img)
-        result = np.sum(diff)
-        
-        if result == 0:
-            st.success("画像が一致しました！「開く」")
-            st.balloons()
-        else:
-            st.error("画像が一致しません。")
+target_img = cv2.imread("image/peach.jpg")
+uploaded_file = st.file_uploader("ドロップしてください",type=["jpg","png","jpeg"])
+# ファイルが存在するかチェック
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    compile_img = np.array(image.convert('RGB'))
+    compile_img = cv2.cvtColor(compile_img, cv2.COLOR_RGB2BGR)
+    height, widht = target_img.shape[:2]
+    compile_img = cv2.resize(compile_img, (widht,height))
+    st.image(compile_img, caption="アップロードされた画像",channels="BGR")
+    diff = cv2.absdiff(target_img, compile_img)
+    result = np.sum(diff)
+    if result < 10000:
+        st.success("正解！！")
+        st.balloons()
+    else:
+        st.error("残念")
+else:
+    st.info("アップロードをしてください")
