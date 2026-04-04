dd3b3acbd2e211d32bec7eac37c422143d658e99
ryuma111-jpg <ryuma20051115@gmail.com>
2026-04-04 16:52:16 +0900

    Update Portfolio.py

--- image_compare_app/Portfolio.py
+++ image_compare_app/Portfolio.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 st.title("画像一致判定プログラム!")
 
-target_img = cv2.imread("image_compare_app/image/peach.jpg")
+target_img = cv2.imread("image/peach.jpg")
 if target_img is None:
     st.error("基準画像が読み込めません")
     st.stop()
