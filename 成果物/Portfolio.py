3aabca35b4b31afae59ab61ab4f1132576d6f502
ryuma111-jpg <ryuma20051115@gmail.com>
2026-04-04 13:14:45 +0900

    Update Portfolio.py

--- 成果物/Portfolio.py
+++ 成果物/Portfolio.py
@@ -6,17 +6,13 @@ import os
 image_path = "成果物/image/peach.jpg"
 
 st.title("画像一致判定プログラム")
-
-# ファイルが存在するかチェック
-
-        
-        if result == 0:
-            st.success("画像が一致しました！「開く」")
-            st.balloons()
-        else:
-            st.error("画像が一致しません。")
-    else:
+if result == 0:
+        st.success("画像が一致しました！「開く」")
+        st.balloons()
+else:
+        st.error("画像が一致しません。")
+else:
         st.error("画像の読み込みに失敗しました（データ形式を確認してください）")
 else:
-    st.error(f"画像ファイルが見つかりません。パスを確認してください: {image_path}")
+        st.error(f"画像ファイルが見つかりません。パスを確認してください: {image_path}")
 
