25fe3976d014b6ae998b61d5f494e77895827673
ryuma111-jpg <ryuma20051115@gmail.com>
2026-04-04 17:01:00 +0900

    Update Portfolio.py

--- image_compare_app/Portfolio.py
+++ image_compare_app/Portfolio.py
@@ -28,8 +28,8 @@ if uploaded_file is not None:
         st.success("正解！！")
         st.balloons()
     else:
-    st.error("残念")
-    st.write(f"差分スコア: {result}")
-    st.write("※差分が小さいほど一致しています")
+        st.error("残念")
+        st.write(f"差分スコア: {result}")
+        st.write("※差分が小さいほど一致しています")
 else:
     st.info("アップロードをしてください")
